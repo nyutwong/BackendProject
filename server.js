76c8e8e0c3e0b06a99a35cac865596d111591ef8
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-02-23 21:12:40 +0700

    feat: connect with mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const car = require("./routes/cars");
+const connectDB = require("./config/db");
+
+dotenv.config({path:"./config/config.env"})
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('App listening on port 3000!');
+connectDB();
+
+const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
 });
 
 app.use("/api/v1/cars",car);
+
+process.on("unhandledRejection",(err,promise)=>{
+    console.log(`ERROR: ${err.message}`);
+    server.close(()=>process.exit(1));
+});
