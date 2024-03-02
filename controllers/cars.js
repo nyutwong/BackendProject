5184079a40390b655bb1c72759a8c6fdcc102ca8
Siratee Topradit <tae.siratee@gmail.com>
2024-03-02 17:31:31 +0700

    feat : Refactor getCars function to support pagination and filtering

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,9 +1,66 @@
 const Car = require("../models/Car");
 
 exports.getCars = async (req, res, next) => {
+    let query;
+
+    const reqQuery = { ...req.query };
+
+    const removeFields = ["select", "sort", "page", "limit"];
+
+    removeFields.forEach((param) => delete reqQuery[param]);
+
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(
+        /\b(gt|gte|lt|lte|in)\b/g,
+        (match) => `$${match}`
+    );
+
+    query = Car.find(JSON.parse(queryStr)).populate("bookings");
+
+    if (req.query.select) {
+        const fields = req.query.select.split(",").join(" ");
+        query = query.select(fields);
+    }
+
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(",").join(" ");
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort("name");
+    }
+
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Car.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
     try {
-        const cars = await Car.find();
-        res.status(200).json({ success: true, count: cars.length, data: cars });
+        const cars = await query;
+
+        const pagination = {};
+
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit,
+            };
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit,
+            };
+        }
+
+        res.status(200).json({
+            success: true,
+            count: cars.length,
+            pagination,
+            data: cars,
+        });
     } catch (err) {
         res.status(400).json({ success: false });
     }
@@ -12,7 +69,6 @@ exports.getCars = async (req, res, next) => {
 exports.createCar = async (req, res, next) => {
     const car = await Car.create(req.body);
     res.status(201).json({ success: true, data: car });
-    
 };
 
 exports.getCar = async (req, res, next) => {
